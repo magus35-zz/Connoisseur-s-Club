06f2bb524ba533da197d3d8f6d6b950c8b4b5104
JOESEIDON <joemarsili@me.com>
2017-06-05 23:59:19 -0700

    Firebase Sign-In
    Users can now sign-in with the credentials they authenticated with the Firebase server! Much excite

--- Connoisseur Club Central/LoginViewController.swift
+++ Connoisseur Club Central/LoginViewController.swift
@@ -43,7 +43,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     //****
     //MARK: View Controller Methods
     //****
-
+    
     
     
     //Register sample users and add a logout observer when view loads
@@ -113,15 +113,16 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     //Attempt to log the user in with the credentials entered if the user taps login
     @IBAction func userDidTapLogin(_ sender: UIButton) {
         if inputsHaveText() { //Case that the user entered text into both text fields
-            let connoisseurCredentials = Credentials(username: usernameField.text!, password: passwordField.text!)
-            if let _ = theServer.authenticateUser(withCredentials: connoisseurCredentials) { //case that the user entered valid credentials
-                statusLabel.text = ""
-                statusLabel.backgroundColor = UIColor(colorLiteralRed: 0, green: 0, blue: 0, alpha: 0)
-                performSegue(withIdentifier: Constants.Segues.login, sender: nil)
-            } else { //case that the user didn't enter valid credentials
-                statusLabel.text = "Please enter valid credentials"
-                statusLabel.backgroundColor = Constants.Colors.loginLabel
-            }//if-else
+            let email = usernameField.text!
+            let password = passwordField.text!
+            Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
+                guard let _ = user, error == nil else {
+                    self.statusLabel.text = error?.localizedDescription
+                    return
+                }
+                
+                self.performSegue(withIdentifier: Constants.Segues.login, sender: nil)
+            })
         } else { //Case that the user didn't enter text into both text fields
             statusLabel.text = "Please enter username and password"
             statusLabel.backgroundColor = Constants.Colors.loginLabel
@@ -192,14 +193,18 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     
     
     func writeUserInfoToDatabase(_ user: Firebase.User, withEmail email: String) {
+        //Create a new change request
         let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
         
+        //Attempt to commit the changes
         changeRequest?.commitChanges() { (error) in
-            if let error = error {
+            if let error = error { //If there was an error, display it in the status label
                 self.statusLabel.text = error.localizedDescription
                 return
             }//if-let
             
+            
+            //Add the user's email to the database
             self.ref.child("users").child(user.uid).setValue(["email": email])
             self.performSegue(withIdentifier: Constants.Segues.login, sender: nil)
         }//commitChanges()
